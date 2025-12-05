eb67850eb2779ed6fe4b506445255c22e3222c0f
FedeJack06 <giacobbefederico6@gmail.com>
2025-12-05 01:35:33 +0100

    fixed point corr + check std dev

--- 3_speedy/fixed_point_corr.py
+++ 3_speedy/fixed_point_corr.py
@@ -9,7 +9,7 @@ plt.rcParams['font.size'] = 12
 
 # Variabile da analizzare
 var_name = 'temp0'
-file = 'DJFmean_clim.nc'
+file = 'DJFmean_nino.nc'
 ds_djf = xr.open_dataset(file)
 da = ds_djf[var_name]
 outDir = "out_img/fix_point_corr/"
@@ -17,16 +17,11 @@ outDir = "out_img/fix_point_corr/"
 # media temporale su tutti i 200 anni
 mean_djf = da.mean(dim='time')
 
-# ============================================
-# 2. CALCOLO ANOMALIE
-# ============================================
 # time_i - MEDIA
 anom_full = da - mean_djf
 
-# ============================================
-# 3. SELEZIONE DEL PUNTO DI RIFERIMENTO
-# ============================================
-# punto correlazione (Pacifico tropicale)
+################################################
+ #PUNTO DI RIFERIMENTO
 lat_target = 0.0
 lon_target = -120.0
 
@@ -36,17 +31,15 @@ print(f"Punto scelto: Lat {lat_target}, Lon {lon_target}")
 # method='nearest' trova il punto di griglia più vicino alle coordinate
 ref_series = anom_full.sel(lat=lat_target, lon=lon_target, method='nearest')
 
-# ============================================
-# 4. CALCOLO DELLA CORRELAZIONE (One-Point Correlation)
-# ============================================
+#############################################
+# One-Point Correlation)
 
 # xr.corr calcola la correlazione di Pearson lungo la dimensione 'time'
 # Confronta la serie del punto (ref_series) con OGNI altro punto della mappa (anom_full)
 corr_map = xr.corr(anom_full, ref_series, dim='time')
 
-# ============================================
-# 5. PLOTTING
-# ============================================
+###########################################
+# PLOTTING
 fig = plt.figure(figsize=(12, 7), constrained_layout=True)
 
 ax2 = fig.add_subplot(1, 1, 1, projection=ccrs.Robinson())
@@ -66,4 +59,22 @@ ax2.legend(loc='lower left')
 plt.colorbar(p2, ax=ax2, label='Coefficiente di Correlazione', shrink=0.8)
 
 plt.savefig(outDir+'corr_'+file[0:3]+'-'+file[8:12]+'_'+f"{lat_target}N_{lon_target}E.png", dpi=150, bbox_inches='tight')
+
+# Calcola la deviazione standard nel tempo (sui 200 ensemble)
+std_dev = anom_full.std(dim='time')
+
+#######################################################
+# Plotta la variabilità
+plt.figure(figsize=(10, 5))
+ax = plt.axes(projection=ccrs.PlateCarree())
+std_dev.plot(ax=ax, transform=ccrs.PlateCarree(), cbar_kwargs={'label': 'Std Dev (K)'})
+ax.coastlines()
+ax.plot(lon_target, lat_target, 'g*', markersize=10) # Il tuo punto
+plt.title("Deviazione Standard sui 200 anni")
+plt.show()
+
+# CONTROLLO NUMERICO
+std_point = std_dev.sel(lat=lat_target, lon=lon_target, method='nearest').item()
+print(f"Deviazione standard nel punto scelto: {std_point}")
+
 plt.show()
